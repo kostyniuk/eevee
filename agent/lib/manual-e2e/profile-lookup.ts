afc108677a83b3c8c5f95f66a0be34383a859048
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-08-15 11:49:39 +0200

    Load profiles concurrently

--- agent/lib/manual-e2e/profile-lookup.ts
+++ agent/lib/manual-e2e/profile-lookup.ts
@@ -2,9 +2,5 @@ export async function loadProfiles(
   ids: readonly string[],
   load: (id: string) => Promise<string>,
 ): Promise<string[]> {
-  const profiles: string[] = [];
-  for (const id of ids) {
-    profiles.push(await load(id));
-  }
-  return profiles;
+  return Promise.all(ids.map((id) => load(id)));
 }
