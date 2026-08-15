63d68d2bfeeb584e11914b75ee22103c857588b0
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-08-15 11:50:41 +0200

    Return report promise directly

--- agent/lib/manual-e2e/report-loader.ts
+++ agent/lib/manual-e2e/report-loader.ts
@@ -1,9 +1,3 @@
 export function loadReport(read: () => Promise<string>): Promise<string> {
-  return new Promise(async (resolve, reject) => {
-    try {
-      resolve(await read());
-    } catch (error) {
-      reject(error);
-    }
-  });
+  return read();
 }
